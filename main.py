95f80a06568ada7ce3ab846f2cd5ce3769aae2ad
statsu1990 <47744788+statsu1990@users.noreply.github.com>
2020-02-13 01:23:37 +0900

    Refactor main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from model import albert, bert
-
-#albert.test()
-#albert.test2()
-#albert.test3()
-#albert.test4()
-#albert.test5()
-
-#albert.check_score()
-#albert.check_score2()
-
+from model import bert
 
 #bert.test1()
 #bert.test2()
@@ -25,34 +15,21 @@ from model import albert, bert
 #bert.test_sepQA_4_2()
 #bert.test_sepQA_4_2_2()
 
-
 #bert.test_sepQA_5_1()
 #bert.test_sepQA_5_1_3()
 #bert.test_sepQA_5_1_3_1()
 #bert.test_sepQA_5_1_3_2()
 
-
 #bert.test_sepQA_2_pair_1()
 #bert.test_sepQA_2_pair_1_2()
-#bert.test_sepQA_2_pair_1_2_copy1()
-#bert.test_sepQA_2_pair_1_2_copy2()
-#bert.test_sepQA_2_pair_1_2_copy3()
-#bert.test_sepQA_2_pair_1_2_copy4()
-#bert.test_sepQA_2_pair_1_2_copy5()
-
-
 #bert.test_sepQA_2_pair_2()
 #bert.test_sepQA_2_pair_2_1()
-
 #bert.test_sepQA_2_pair_3()
-
-#bert.test_sepQA_2_list_1()
 
 #bert.check_score_test_sepQA_2()
 #bert.check_score_test_sepQA_3()
 #bert.check_score_test_sepQA_4()
 #bert.check_score_test_sepQA_5()
-
 
 #bert.test_sepQA_4_1_2_comb3()
 #bert.test_sepQA_4_1_2_comb2_v2()
@@ -71,7 +48,6 @@ bert.test_sepQA_4_1_2_comb5_v3()
 
 
 #bert.test_sepQA_4_1_2_comb1_v2()
-
 #bert.test_sepQA_4_2_1_comb4()
 #bert.test_sepQA_4_2_1_comb4_2()
 #bert.test_sepQA_4_2_1_comb5_1()
