185ff94edadc11f73c0a4949a5fe3c95d5c56b74
statsu1990 <47744788+statsu1990@users.noreply.github.com>
2020-02-13 01:27:53 +0900

    Refactor

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 
 PATH = '../input/google-quest-challenge/'
 
-PRETRAINED_MODEL = 'albert-large-v2'
-TOKENIZER_PATH = '../input/transformers/' + PRETRAINED_MODEL + '/tokenizer/'
-PRETRAINED_MODEL_PATH = '../input/transformers/' + PRETRAINED_MODEL + '/pretrained_model/'
-
 #BERT_PRETRAINED_MODEL = 'bert-base-cased'
 BERT_PRETRAINED_MODEL = 'bert-base-uncased'
 BERT_TOKENIZER_PATH = '../input/transformers/' + BERT_PRETRAINED_MODEL + '/tokenizer/'
